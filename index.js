2665e3a02bdaaeece8268c1eafe1d9f97fe9b682
david-rai <davidkhaling123@gmail.com>
2025-04-18 12:17:39 +0545

    jsonwebtoken and the cookies intregrated

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
 const cookieParser = require('cookie-parser');
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt")
 const express = require('express');
 const app = express();
-const path=require("path")
-const PORT =  1111;
+const path = require("path")
+const PORT = 1111;
 require("dotenv").config()
-const db=require("./models/db.js")
+const db = require("./models/db.js")
+const jwt=require("jsonwebtoken")
 
 //middlewares
 app.use(cookieParser())
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 
 //view engine
-app.set('view engine',"ejs")
-app.set("views",path.resolve("views"))
+app.set('view engine', "ejs")
+app.set("views", path.resolve("views"))
 
+
+//home page registration
 app.get('/', (req, res) => {
 
   res.render("register")
 });
 
-app.post('/register',async (req,res)=>{
-const {email,password}=req.body;
+//registration route
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
 
-//hashing the password
-bcrypt.genSalt(10, function(err, salt) {
-  bcrypt.hash(password, salt, function(err, hash) {
+const token=jwt.sign({email},"secret")
+res.cookie("token",token)
+console.log(token)
+
+  //hashing the password
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
       // Store hash in your password DB.
-      console.log(hash)
+      const query = "insert into users (email,password) values (?,?)"
+      const results = await db.execute(query, [email, hash])
+    });
   });
-});
 
-const query="insert into users (email,password) values (?,?)"
-const results=await db.execute(query,[email,password])
-console.log(results)
 
   res.render("register")
+})
+
+//get cookie
+app.get('/read',(req,res)=>{
+const token=req.cookies.token
+const data=jwt.verify(token,"secret")
+res.json({token,data})
 })
 
 app.listen(PORT, () => {

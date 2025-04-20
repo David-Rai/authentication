e151c6f49cf1dd771e033d0073516d8e4bf5a096
david-rai <davidkhaling123@gmail.com>
2025-04-20 07:33:18 +0545

    fix:conflicts fixed & final commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,37 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-<<<<<<< HEAD
     const token = req.cookies.token;
     
-    if (!token) {
-    res.redirect("/")
-    }
-=======
-  const token = req.cookies.token;
->>>>>>> 323ffa6d8f829c52ede939045fac1659a262a9f9
 
   if (!token) {
     return res.redirect("/");
   }
 
-<<<<<<< HEAD
-    req.user = data
+  const secret=process.env.SECRET
+  const data=jwt.verify(token,secret)
 
-    next()
-=======
-  try {
-    const secretCode = process.env.SECRET;
-    const data = jwt.verify(token, secretCode);
->>>>>>> 323ffa6d8f829c52ede939045fac1659a262a9f9
 
     req.user = data;
     next();
-  } 
-  catch (err) {
-    console.error("JWT Error:", err.message);
-    return res.redirect("/");
-  }
-};
+
+}
 
 module.exports = auth;
